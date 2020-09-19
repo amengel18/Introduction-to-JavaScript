0f95e2a5e4e537c0ff26faf21fba063c4d01437a
Angela Engel <amengel.ae@icloud.com>
2020-09-18 22:09:59 -0600

    Task 1 A and B

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
 
+var votingAge = 18 
 
-
-
+if (votingAge > 18) {console.log("true")} 
 
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
 
+var name = "Anthony"  
 
-
-
+var name = "Tony"
 
 //Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
 
