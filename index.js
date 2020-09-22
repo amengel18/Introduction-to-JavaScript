90128b6c9fae89c123bb153c4a56346cb79ec378
Angela Engel <amengel.ae@icloud.com>
2020-09-22 17:21:16 -0600

    fixed problem

--- index.js
+++ index.js
@@ -182,11 +182,11 @@ function annoyingSong(bottles) {
 function gradeCalc(score){
     if (score < 60){
         return 'F';
-    } else if (score >= 60 && < 70){
+    } else if (score >= 60 && score < 70){
         return 'D';
-    } else if (score >= 70 && < 80){
+    } else if (score >= 70 && score < 80){
         return 'C';
-    } else if (score >= 80 && < 90){
+    } else if (score >= 80 && score < 90){
         return 'B';
     } else if (score >= 90){
         return 'A';
